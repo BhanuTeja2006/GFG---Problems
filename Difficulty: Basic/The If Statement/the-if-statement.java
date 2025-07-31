fccaa7800fd7be4a0e3b6ed0b341725a02bbaf88
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-07-31 12:16:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The If Statement/the-if-statement.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+class Solution {
+    public static void utility(int number) {
+        if( number > 100){
+            System.out.println("Big\nNumber");
+        }
+        else{
+            System.out.println("Number");
+        }
+
+    }
+}
