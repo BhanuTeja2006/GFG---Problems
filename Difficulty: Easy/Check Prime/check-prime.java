9004bcaf0dc931d86f87e6b3aa13076db700718a
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-08-23 15:03:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Prime/check-prime.java
+++ Difficulty: Easy/Check Prime/check-prime.java
@@ -1,15 +1,21 @@
-class Solution {
-    public static boolean prime(int n) {
-        if(n==1) {
-            return false;
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
         }
-        // Write your code here
-        for(int i=2;i<n;i++){
-             if(n%i==0){
-                 return false;
-             }
-             
+
+        if (count == 0) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
         }
-        return true;
     }
 }
