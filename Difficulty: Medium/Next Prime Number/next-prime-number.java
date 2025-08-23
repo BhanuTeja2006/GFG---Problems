95c8e0ea97494ca2a0f092545e72adcf8a81b053
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-08-23 14:06:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Prime Number/next-prime-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    static boolean prime(int n){
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static int nextPrime(int n) {
+        int i = n;
+        for(i=n+1;i<=500;i++){
+            boolean isPrime = false;
+            isPrime = prime(i);
+            if(isPrime){
+                return i;
+            }
+            
+        }
+        return i;
+    }
+}
