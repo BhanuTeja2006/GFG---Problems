b19781c8271f81266b29b712765b42e63e640573
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-09-04 14:32:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Evaluate Formulae/evaluate-formulae.java
+++ Difficulty: Basic/Evaluate Formulae/evaluate-formulae.java
@@ -1,9 +1,11 @@
+// User function Template for Java
 class Solution {
-    public int calculate(int a, int b, int c, int d) {
-        // code here
-        int f = (a+b)/c;
-        int g = f+d;
-        return  g;
-        
+    public static void evalute(int a, int b, int c, int d) {
+        // Complete the code above that stores the output of the expression
+        int res =(a+b)/c + d;
+            // Complete the code above that stores the output of the expression
+
+            // This prints the output. Don't change it!
+            System.out.println(res);
     }
 }
