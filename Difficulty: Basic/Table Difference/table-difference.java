49afe4ce5708580c3708b7ea0831d5bc04d3e419
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-09-08 10:15:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Table Difference/table-difference.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+class Solution {
+    public static void difference(int n1, int n2) {
+        for(int i=1;i<=10;i++){
+            int num1=n1*i;
+            int num2=n2*i;
+            int dif=num1-num2;
+            System.out.print(dif+" ");
+            }
+
+        // Write your code here
+        
+    }
+}
