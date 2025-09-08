a315fee5a8149bb8d5f3183618bf359ad0f6a85f
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-09-08 20:11:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Right Angle Triangle/right-angle-triangle.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+class Solution {
+    public static void triangleWall(int s) {
+        for (int i = 1; i <= s; i++) {
+            for (int j = 1; j <= i; j++) {
+                    System.out.print("* ");
+                
+            }
+            System.out.println();
+        }
+        
+    }
+}
