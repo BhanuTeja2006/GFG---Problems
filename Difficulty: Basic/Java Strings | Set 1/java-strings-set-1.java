8bcaea0869d6cdfb00821c4826ee27969b666623
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-09-08 10:12:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java Strings | Set 1/java-strings-set-1.java
@@ -0,0 +1,9 @@
+// User function template for Java
+
+class Solution {
+    static String conRevstr(String S1, String S2) {
+         String S=S1+S2;
+        return new StringBuilder(S).reverse().toString();
+        
+    }
+}
