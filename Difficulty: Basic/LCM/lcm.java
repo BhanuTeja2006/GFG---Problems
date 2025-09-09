c2204ecf86004cc4adce9058a99b7b6f98bf8533
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-09-09 11:37:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM/lcm.java
+++ Difficulty: Basic/LCM/lcm.java
@@ -1,16 +1,18 @@
-// User function Template for Java
-class Solution {
-    public static int LCM(int a, int b) {
+import java.util.Scanner;
 
-        // write your code here
+public class Solution {
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // take input 
         int p=a,q=b;
         while(b!=0){
             int temp =b;
             b=a%b;
             a=temp;
         }
-        return (p*q)/a;
-
-        // return LCM of a and b
+        System.out.print((p*q)/a);
     }
 }
+
