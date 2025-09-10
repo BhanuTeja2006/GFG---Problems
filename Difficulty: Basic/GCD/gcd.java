14e457a8517e418cbf1aa445a376b18e26860b38
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-09-10 10:37:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD/gcd.java
+++ Difficulty: Basic/GCD/gcd.java
@@ -1,15 +1,10 @@
-// User function Template for Java
 class Solution {
-    public static int gcd(int a, int b) {
-        int gcd=1;
-        int min=Math.min(a,b);
-        for(int i=min;i>1;i--){
-            if(a%i==0 && b%i==0){
-                gcd=i;
-                break;
-            }
+    public int computeGCD(int a, int b) {
+        if(b==0){
+            return a;
         }
-        return gcd;
-        // code here to calculate and return gcd of a and b
+        return computeGCD(b,a%b);
+        
+        
     }
 }
