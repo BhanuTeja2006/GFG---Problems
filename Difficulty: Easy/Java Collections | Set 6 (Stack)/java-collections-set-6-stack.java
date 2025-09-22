ec9fe2af00cbfe8ebe7912064a2e9b1136c18417
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-09-22 11:18:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Java Collections | Set 6 (Stack)/java-collections-set-6-stack.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+
+class Solution {
+    static Stack<Integer> deleteMid(int n, Stack<Integer> st) {
+        int mid = (n-1)/2;
+        st.remove(mid);
+    return st;
+        // code here
+        
+    }
+}
