f53318c2667b69814494f0f9b91878274ca68a24
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-09-22 10:29:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Java Collections | Set 7 (LinkedList)/java-collections-set-7-linked-list.java
@@ -0,0 +1,11 @@
+class GfG {
+    public void deleteNode(Node node) {
+        if(node.next==null){
+            node = null;
+        }
+        else{
+            node.data=node.next.data;
+            node.next=node.next.next;
+        }
+    }
+}
