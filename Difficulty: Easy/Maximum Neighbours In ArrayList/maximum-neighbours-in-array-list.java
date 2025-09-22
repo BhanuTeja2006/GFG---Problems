2baac398b313158469eff799046786ef961c376a
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-09-22 10:09:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum Neighbours In ArrayList/maximum-neighbours-in-array-list.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static ArrayList<Integer> maxNeighbour(ArrayList<Integer> arr) {
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i=0;i<arr.size()-1;i++){
+            res.add(Math.max(arr.get(i),arr.get(i+1)));
+        }
+
+        return res;
+    }
+}
