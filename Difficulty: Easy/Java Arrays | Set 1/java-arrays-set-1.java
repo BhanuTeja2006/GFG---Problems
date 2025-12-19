e3dc4409b0c9b1ce6d1889eb378c8faab972d1f4
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2025-12-19 13:41:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Java Arrays | Set 1/java-arrays-set-1.java
+++ Difficulty: Easy/Java Arrays | Set 1/java-arrays-set-1.java
@@ -3,8 +3,11 @@ class Solution {
     public String average(int arr[]) {
         // code here
         int sum=0;
-        for(int n:arr) sum+=n;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
         double avg=(double)sum/arr.length;
         return String.format("%.2f",avg);
+        
     }
 }
