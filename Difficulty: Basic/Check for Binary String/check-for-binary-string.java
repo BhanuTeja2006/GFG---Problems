a7f6631da2307d892d25b23832cd02fb70ffac3c
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2026-01-09 11:36:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Check for Binary String/check-for-binary-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    boolean isBinary(String s) {
+        for(char c:s.toCharArray()){
+            if(c !='0' && c !='1'){
+                return false;
+            }
+        }
+        return true;
+        // Your code here
+        
+    }
+}
