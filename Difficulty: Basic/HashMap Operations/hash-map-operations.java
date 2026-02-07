695440bfa2130aa2e589d145d7e09cc9e516e164
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2026-02-07 11:09:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/HashMap Operations/hash-map-operations.java
@@ -0,0 +1,16 @@
+/*You are required to complete below methods */
+class GfG {
+    void add_Value(HashMap<Integer, Integer> hm, int x, int y) {
+        hm.put(x,y);
+    }
+    int find_value(HashMap<Integer, Integer> hm, int x) {
+        if(hm.containsKey(x)) return hm.get(x);
+        return -1;
+    }
+    int getSize(HashMap<Integer, Integer> hm) {
+        return hm.size();
+    }
+    void removeKey(HashMap<Integer, Integer> hm, int x) {
+        hm.remove(x);
+    }
+}
