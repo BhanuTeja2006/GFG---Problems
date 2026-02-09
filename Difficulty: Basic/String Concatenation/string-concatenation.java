e61b2bcad59d62116059ee87c529d0ba1cff7ecb
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2026-02-09 18:26:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/String Concatenation/string-concatenation.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+class Solution {
+    public static String concatenateStrings(ArrayList<String> arr) {
+        // Your code here
+        String str = " ";
+        for(String i : arr){
+            str+=i;
+        }
+        return str;
+    }
+}
