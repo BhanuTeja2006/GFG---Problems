77f9b1c115f607217dd798d31b44817b27d33d10
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2026-02-11 13:42:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Multiply two strings/multiply-two-strings.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+class Solution {
+    public String multiplyStrings(String s1, String s2) {
+        BigInteger n1 = new BigInteger(s1);
+        BigInteger n2 = new BigInteger(s2);
+        BigInteger num = n1.multiply(n2);
+        return num.toString();
+    }
+}
