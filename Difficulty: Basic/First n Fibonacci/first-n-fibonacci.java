cdc259d7d893413b98f790c76ea36e008d8f24c6
BhanuTeja2006 <bhanutejareddy07@gmail.com>
2026-03-02 17:40:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/First n Fibonacci/first-n-fibonacci.java
+++ Difficulty: Basic/First n Fibonacci/first-n-fibonacci.java
@@ -11,7 +11,7 @@ class Solution {
         res[0] = 0;
         res[1] = 1;
         for(int i=2;i<n;i++){
-            res[i] = res[i-1]+ res[i-2];
+            res[i] = res[i-1]+res[i-2];
         }
         return res;
     }
